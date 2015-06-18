bb13168847e61cd4ac4bcf852ca6184af709f6ab
David Valleau <davidvalleau@gmail.com>
2015-06-18 10:48:58 -0400

    Cleaned up dictionary and added comments

--- algorithms/strings/sherlock_and_anagrams.py
+++ algorithms/strings/sherlock_and_anagrams.py
@@ -1,37 +1,18 @@
 from collections import defaultdict
 
+# Map each character to a unique prime number
 primes = {
-    'a' : 2,
-    'b' : 3,
-    'c' : 5,
-    'd' : 7,
-    'e' : 11,
-    'f' : 13,
-    'g' : 17,
-    'h' : 19,
-    'i' : 23,
-    'j' : 29,
-    'k' : 31,
-    'l' : 37,
-    'm' : 41,
-    'n' : 43,
-    'o' : 47,
-    'p' : 53,
-    'q' : 59,
-    'r' : 67,
-    's' : 71,
-    't' : 73,
-    'u' : 79,
-    'v' : 83,
-    'w' : 89,
-    'x' : 97,
-    'y' : 101,
-    'z' : 103
+    'a' : 2, 'b' : 3, 'c' : 5, 'd' : 7, 'e' : 11, 'f' : 13, 'g' : 17,
+    'h' : 19, 'i' : 23, 'j' : 29, 'k' : 31, 'l' : 37, 'm' : 41, 'n' : 43,
+    'o' : 47, 'p' : 53, 'q' : 59, 'r' : 67, 's' : 71, 't' : 73, 'u' : 79,
+    'v' : 83, 'w' : 89, 'x' : 97, 'y' : 101, 'z' : 103
 }
 
+# Used to return the number of unique pairs, based on occurrences of a string
 def summation(n):
     return (n * (n - 1)) / 2
 
+# Use the primes map to generate a unique key for every string
 def get_key(s):
     prod = 1;
     for c in s:
@@ -41,6 +22,8 @@ def get_key(s):
 def count_pairs(s):
     anagrams = {}
     anagrams = defaultdict(lambda: 0, anagrams)
+
+    # use a nested loop to iterate through all slices of the string
     for i in range(0, len(s)):
         for j in range(i, len(s)):
             anagrams[get_key(s[i:j+1])] += 1

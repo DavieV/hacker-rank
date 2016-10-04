aca1587d764ba765855d56eb6be759c0c414ea4b
Quinn Perfetto <Quinny@users.noreply.github.com>
2016-10-04 12:36:07 -0400

    Update Lonely Ints to be more functional.
    Tested on hacker rank, this cod is 100% AC.

--- algorithms/search/lonely_int.cpp
+++ algorithms/search/lonely_int.cpp
@@ -1,16 +1,17 @@
+#include <functional>
 #include <iostream>
+#include <iterator>
+#include <numeric>
+#include <vector>
 
 int main() {
-    int n;
-    int cur = 0;
+  int n;
+  std::cin >> n;
 
-    std::cin >> n;
-    int tmp;
-    for (int i = 0; i < n; ++i) {
-        std::cin >> tmp;
-        cur ^= tmp;
-    }
-
-    std::cout << cur << std::endl;
-    return 0;
+  std::vector<int> ints;
+  std::copy(std::istream_iterator<int>(std::cin), std::istream_iterator<int>(),
+            std::back_inserter(ints));
+  auto lonely = std::accumulate(ints.begin() + 1, ints.end(), *ints.begin(),
+                                std::bit_xor<int>());
+  std::cout << lonely << std::endl;
 }
